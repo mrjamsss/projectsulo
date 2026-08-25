13d35aa52436801a23d6a822d9115634c38e20af
mrjamsss <joraldmasilsevilla0806@gmail.com>
2026-08-25 22:01:02 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'projectsulo',
+  webDir: 'www'
+};
+
+export default config;
